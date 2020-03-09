b7b061dfdcfeb8b2b33b2e9387a11c7e544f90fd
HoaDT <hoadt.21.it@gmail.com>
2020-03-09 23:50:07 +0700

    add lib

--- NuMusic/NuMusic/App.xaml.cs
+++ NuMusic/NuMusic/App.xaml.cs
@@ -3,10 +3,13 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using NuMusic.Services;
 using NuMusic.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism;
 
 namespace NuMusic
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
 
         public App()
@@ -14,19 +17,26 @@ namespace NuMusic
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+
         }
 
-        protected override void OnStart()
+        public App(IPlatformInitializer initializer = null) : base(initializer)
         {
         }
 
-        protected override void OnSleep()
+        protected override void OnInitialized()
         {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(AppSettings.SyncFusionLicenseKey);
+            InitializeComponent();
+            // Đặt ngôn ngữ cho App dựa vào property đã lưu, nếu không có thì đặt tiếng Việt làm mặc định
+            SetAppLanguage();
         }
 
-        protected override void OnResume()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+
+
         }
     }
 }
